abd04b1e6d4383b519e1096d481c22ec4b6ac5b7
Rogalla von Bieberstein Bruno <Bruno@Bruno.local>
2018-06-05 18:42:32 +0200

    ich glaube die decimals sind an den beiden fails schuld, aber die bekomme ich nicht zum Funktionieren

--- numerize/numerize.py
+++ numerize/numerize.py
@@ -1,70 +1,36 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# code nicht geändert, nur bash geübt.
-import sys
+"""
+Created on Tue Jun  5 13:49:15 2018
+
+@author: Bruno
+"""
+
 from decimal import Decimal
 
+len_destinator = {1 : 1, 2 : 1, 3 : 1, 4 : 1000, 5 : 1000, 6 : 1000, 7 : 1000000, 8 : 1000000, 9 : 1000000, 10 : 1000000000,11 : 1000000000, 12 : 1000000000, 13 : 1000000000000,14 : 1000000000000, 15 : 1000000000000}
+KMBT = {1 : '', 2 : '', 3 : '', 4 : 'K', 5 : 'K', 6 : 'K', 7 : 'M', 8 : 'M', 9 : 'M', 10 : 'B', 11 : 'B', 12 : 'B', 13 : 'T',14 : 'T', 15 : 'T'}
+
+
 def round_num(n, decimals):
-    '''
-    Params:
-    n - number to round
-    decimals - number of decimal places to round to
-    Round number to 2 decimal places
-    For example:
-    10.0 -> 10
-    10.222 -> 10.22
-    '''
     return n.to_integral() if n == n.to_integral() else round(n.normalize(), decimals)
 
 def drop_zero(n):
-    '''
-    Drop trailing 0s
-    For example:
-    10.100 -> 10.1
-    '''
     n = str(n)
     return n.rstrip('0').rstrip('.') if '.' in n else n
 
 def numerize(n, decimals=2):
-    '''
-    Params:
-    n - number to be numerized
-    decimals - number of decimal places to round to
-    Converts numbers like:
-    1,000 -> 1K
-    1,000,000 -> 1M
-    1,000,000,000 -> 1B
-    1,000,000,000,000 -> 1T
-    '''
+    n_neg = int(n)
+    n_int = abs(Decimal(n))
     is_negative_string = ""
-    if n < 0:
+    if n_neg < 0:
         is_negative_string = "-"
-    n = abs(Decimal(n))
-    if n < 1000:
-        return is_negative_string + str(drop_zero(round_num(n, decimals)))
-    elif n >= 1000 and n < 1000000:
-        if n % 1000 == 0:
-            return is_negative_string + str(int(n / 1000)) + "K"
-        else:
-            n = n / 1000
-            return is_negative_string + str(drop_zero(round_num(n, decimals))) + "K"
-    elif n >= 1000000 and n < 1000000000:
-        if n % 1000000 == 0:
-            return is_negative_string + str(int(n / 1000000)) + "M"
-        else:
-            n = n / 1000000
-            return is_negative_string + str(drop_zero(round_num(n, decimals))) + "M"
-    elif n >= 1000000000 and n < 1000000000000:
-        if n % 1000000000 == 0:
-            return is_negative_string + str(int(n / 1000000000)) + "B"
-        else:
-            n = n / 1000000000
-            return is_negative_string + str(drop_zero(round_num(n, decimals))) + "B"
-    elif n >= 1000000000000 and n < 1000000000000000:
-        if n % 1000000000000 == 0:
-            return is_negative_string + str(int(n / 1000000000000)) + "T"
-        else:
-            n = n / 1000000000000
-            return is_negative_string + str(drop_zero(round_num(n, decimals))) + "T"
+    if len(str(n_int)) <= 15:
+        return is_negative_string + str(drop_zero(round_num(n_int / len_destinator[len(str(n_int))], decimals))) + KMBT[len(str(n_int))]
     else:
-        return is_negative_string + str(n)
+        return is_negative_string + str(n_int)
+
+if __name__ == '__main__':
+    n = input('Zahl: ')
+    decimals = input('Decimals: ')
+    print(numerize(n, decimals))
